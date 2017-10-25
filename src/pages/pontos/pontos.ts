8f83416acfd9435d0147091932a54cafae2ade10
Daniele Reis <danielesreis@outlook.com>
2017-10-25 15:07:10 -0200

    commit

--- src/pages/pontos/pontos.ts
+++ src/pages/pontos/pontos.ts
@@ -17,9 +17,4 @@ export class PontosPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PontosPage');
-  }
-
 }
